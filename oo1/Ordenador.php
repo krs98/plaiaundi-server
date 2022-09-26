9b875b6aa02f20005987a60b4e5674cff2a42d54
chris <christian.crespoo98@gmail.com>
2022-09-26 12:28:48 +0200

    add

--- oo1/Ordenador.php
+++ oo1/Ordenador.php
@@ -11,6 +11,7 @@ class Ordenador {
     public function __construct(
         private string $so, 
         private string $codHz,
+        private bool $esSobremesa,
         private Despacho $despacho
     ) { 
         $this->despacho->addOrdenadores($this);
